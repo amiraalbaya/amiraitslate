f960c4455d196d4ab3b84da51b3c2416cd3fc19f
islab <islab@islabs-MacBook-Pro.local>
2016-12-03 19:30:49 +0200

    please work

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   delete 'session/destroy' => 'session#destoy'
   get 'session/destroy' => 'session#destoy', as: :logout
   get 'users/new' => 'users#new', as: :signup
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
